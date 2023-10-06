f40890fd9b076d0d8363be2efedd549a0fc780b5
DenisPepper <thech@yandex.ru>
2023-10-06 20:08:32 +0300

    Добавит глобальные стили

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction } from '@remix-run/node';
 import {
   Links,
   LiveReload,
@@ -7,18 +6,26 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react';
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import styles from '~/styles/main.css';
+
+export const links: LinksFunction = () => {
+  // `links` returns an array of objects whose
+  // properties map to the `<link />` component props
+  /*{ rel: "icon", href: "/favicon.ico" },
+    { rel: "apple-touch-icon", href: "/logo192.png" },
+    { rel: "manifest", href: "/manifest.json" },
+    { rel: "stylesheet", href: stylesheetUrl },*/
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 export default function App() {
   return (
-    <html lang="ru">
+    <html lang='ru'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
